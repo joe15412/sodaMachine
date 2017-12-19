6e658be79891505c3f12fbe841d52f74eac3ae98
joe raleigh <joseph.m.raleigh@gmail.com>
2017-12-19 12:01:42 -0600

    outoftime push

--- sodaMachine/UserInterface.cs
+++ sodaMachine/UserInterface.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace sodaMachine
 {
-   public class UserInterface
+    public class UserInterface
     //let them select soda type
     //tell them how much it cost
     //show user money available
@@ -16,53 +16,69 @@ namespace sodaMachine
     //if no change in machine, cancel order
 
     {
-        public static displayWelcome()
+        private static int sodaType;
+
+        public static string grapeSodaQty { get; set; }
+        public static string LemonSodaQty { get; set; }
+        public static string orangeSodaQty { get; set; }
+
+        public static void displayWelcome()
         {
             Console.WriteLine("Welcome to the vending machine!");
             Console.WriteLine("What type of soda do you want?");
-            Console.WriteLine("G: Grape");
-            Console.WriteLine("L: Lemon");
-            Console.WriteLine("O: Orange");
+            Console.WriteLine("[1}: Grape");
+            Console.WriteLine("[2]: Lemon");
+            Console.WriteLine("[3]: Orange");
 
-           string value = Console.ReadLine();
+            string value = Console.ReadLine();
             switch (value)
             {
-             case G:
-                    Console.WriteLine("There are" + Inventory.grapeSoda + "cans left.")
- 
-
-
-              case L;
-                    Console.WriteLine("There are" + Inventory.lemonSoda + "cans left")
- 
-
-
-              case O;
-                    Console.WriteLine("There are" + Inventory.orangeSoda + "cans left")
- 
-
-
-
-               default: "invalid entry";
+                case 1:
+                    Console.WriteLine("There are" + grapeSodaQty + "cans left.");
+                    Console.WriteLine("Grape costs 60 cents.");
+                    return;
+                case 2:
+                    Console.WriteLine("There are" + LemonSodaQty + "cans left.");
+                    Console.WriteLine("Lemon costs 6 cents.");
+                    return;
+                case 3:
+                    Console.WriteLine("There are" + orangeSodaQty + "cans left.");
+                    Console.WriteLine("Orange costs 35 cents.");
+                    return;
             }
 
 
+            default: "Invalid entry;";
+
+            // types available - quanitity wanted
+            //if more then 5 of type then error message
+
+            Console.WriteLine("What type do you want?");
+            string type = Console.ReadLine();
 
 
-            }
+            Console.WriteLine("How Many do you want?");
+            int qty = Console.Read();
+
+            //how do you want to pay for it
+            //show listofcoins available
+            //add getters and setters
+
+            Console.WriteLine("How would you like to pay?");
+
+
+
         }
-                
-                
-               
-                )
-
-
-
-
-
-                
-
-        }
-
     }
+
+
+
+
+
+
+
+
+
+
+
 }
